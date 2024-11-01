20d81558c9fe43a3ea59ccd001dc372855eb8318
François-Xavier Coindreau <fx.coindreau@aeveil.ch>
2024-11-01 13:37:40 +0100

    Text controller

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -19,20 +19,31 @@ class _HomePageState extends State<HomePage>
 {
   String _simple = "";
   String _afterSubmit = "";
+  late TextEditingController _controller;
 
   final FocusNode _focus = FocusNode(); // cette fonction sert à mettre le focus sur un node
   final FocusNode _next = FocusNode();
+  final FocusNode _forButton = FocusNode();
+
+  final _formKey = GlobalKey<FormState>();
 
   void unfocus (FocusNode focusNode)
   {
     focusNode.unfocus();
   }
 
-  //@override
-  //void initState()
-  //{
-  //  super.initState();
-  //}
+  @override
+  void initState()
+  {
+    super.initState();
+    _controller = TextEditingController(text : "Hello World !");
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,58 +56,92 @@ class _HomePageState extends State<HomePage>
       backgroundColor: Colors.lightGreenAccent,
 
       body: InkWell(
-
         onTap: () {
-          unfocus(_focus); // chaque fois que je clique sur le body cela enleve le focus
+          // chaque fois que je clique sur le body cela enleve le focus
+          unfocus(_focus);
         },
         child: Center(
-          child: Column (
-            children: [
-              Text("Text field simple : $_simple"),
-              Text("After submit : $_afterSubmit"),
-              Row(
-                children: [
-                  Expanded(
-                    child:
-
-                    TextField(
-                      focusNode: _focus,
-                      textAlign: TextAlign.center,
-                      keyboardType: TextInputType.number,
-                      onChanged: (newStr)
-                      {
-                        setState(() {
-                          _simple = newStr;
-                        });
-                      },
-                      onSubmitted: (newStr)
-                      {
-                        setState(() {
-                          _afterSubmit = newStr;
-                        });
-                      },
-                    ),
+          child: Padding (
+            padding: const EdgeInsets.all(42),
+            child : Column (
+              children: [
+                Text("Text field simple : $_simple"),
+                Text("After submit : $_afterSubmit"),
+                TextField(
+                  focusNode: _focus,
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.number,
+                  onChanged: (newStr)
+                  {
+                    setState(() {
+                      _simple = newStr;
+                    });
+                  },
+                  onSubmitted: (newStr)
+                  {
+                    setState(() {
+                      _afterSubmit = newStr;
+                    });
+                  },
+                ),
+                IconButton (
+                    onPressed: (){FocusScope.of(context).requestFocus(_next);},
+                    icon: const Icon(Icons.arrow_downward)
+                ),
+                TextField(
+                  focusNode: _next,
+                  decoration: InputDecoration(
+                    border : OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(42),
+                    )
                   ),
-                  IconButton(
-                      onPressed: (){FocusScope.of(context).requestFocus(_next);},
-                      icon: const Icon(Icons.icecream)
+                ),
+                const SizedBox(height: 98),
+                TextField(
+                  focusNode: _forButton,
+                  controller: _controller,
+                  decoration: const InputDecoration( border: OutlineInputBorder() ),
+                  onSubmitted: (newValue) {
+                    setState(() {
+                    });
+                  },
+                ),
+                ElevatedButton(
+                    onPressed: (() => setState(() {unfocus(_forButton);})),
+                    child: const Text("Reveal the text"),
+                ),
+                Text(_controller.text),
+                Form(
+                  key: _formKey,
+                  child: Column (
+                    children: [
+                      TextFormField(
+                        validator : (value) {
+                          if (value == null || value.isEmpty) {
+                            return "erreur";
+                          }
+                          else {
+                            null;
+                          }
+                        },
+                      ),
+                      TextButton(
+                          onPressed: ()
+                          {
+                            if (_formKey.currentState!.validate()) {
+                              FocusScope.of(context).unfocus();
+                            } else {
+                              print("Nope");
+                            };
+                          },
+                          child: const Text("Valider")
+                      )
+                    ],
                   )
-                ],
-              ),
-                    const SizedBox(height: 24),
-                    Padding(
-                        padding: const EdgeInsets.all(8),
-                        child : TextField(
-                            focusNode: _next,
-                            decoration: InputDecoration(
-                              border : OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(42),
-                              )
-                            ),
-                        ),
-                    ),
-            ],
-          ),
+                )
+              ],
+            ),
+          )
         ),
       )
     );
