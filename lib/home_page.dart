da7fc5cb1ce2dda474c9959c14ea00bfc4dda378
François-Xavier Coindreau <fx.coindreau@aeveil.ch>
2024-11-01 15:20:47 +0100

    Les sliders

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,6 +6,7 @@ class HomePage extends StatefulWidget
 {
   final String title; // Attribut
 
+
   const HomePage ({super.key, required this.title}); // Constructeur
 
   @override
@@ -17,131 +18,61 @@ class HomePage extends StatefulWidget
 
 class _HomePageState extends State<HomePage>
 {
-  String _simple = "";
-  String _afterSubmit = "";
-  late TextEditingController _controller;
+  bool isOn = true;
+  double _min = 0;
+  double _max = 6;
+  double _current = 3;
 
-  final FocusNode _focus = FocusNode(); // cette fonction sert à mettre le focus sur un node
-  final FocusNode _next = FocusNode();
-  final FocusNode _forButton = FocusNode();
-
-  final _formKey = GlobalKey<FormState>();
-
-  void unfocus (FocusNode focusNode)
-  {
-    focusNode.unfocus();
-  }
-
-  @override
-  void initState()
-  {
-    super.initState();
-    _controller = TextEditingController(text : "Hello World !");
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: AppBar(title: Text(widget.title)),
+      backgroundColor: isOn ? Colors.lightGreenAccent : Colors.blueGrey,
 
-      ),
-
-      backgroundColor: Colors.lightGreenAccent,
-
-      body: InkWell(
-        onTap: () {
-          // chaque fois que je clique sur le body cela enleve le focus
-          unfocus(_focus);
-        },
-        child: Center(
-          child: Padding (
-            padding: const EdgeInsets.all(42),
-            child : Column (
+      body: Center (
+        child : Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Text field simple : $_simple"),
-                Text("After submit : $_afterSubmit"),
-                TextField(
-                  focusNode: _focus,
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.number,
-                  onChanged: (newStr)
-                  {
-                    setState(() {
-                      _simple = newStr;
-                    });
-                  },
-                  onSubmitted: (newStr)
-                  {
-                    setState(() {
-                      _afterSubmit = newStr;
-                    });
-                  },
-                ),
-                IconButton (
-                    onPressed: (){FocusScope.of(context).requestFocus(_next);},
-                    icon: const Icon(Icons.arrow_downward)
-                ),
-                TextField(
-                  focusNode: _next,
-                  decoration: InputDecoration(
-                    border : OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(42),
-                    )
-                  ),
-                ),
-                TextField(
-                  focusNode: _forButton,
-                  controller: _controller,
-                  decoration: const InputDecoration( border: OutlineInputBorder() ),
-                  onSubmitted: (newValue) {
-                    setState(() {
-                    });
-                  },
-                ),
-                ElevatedButton(
-                    onPressed: (() => setState(() {unfocus(_forButton);})),
-                    child: const Text("Reveal the text"),
-                ),
-                Text(_controller.text),
-                Form(
-                  key: _formKey,
-                  child: Column (
-                    children: [
-                      TextFormField(
-                        validator : (value) {
-                          if (value == null || value.isEmpty) {
-                            return "erreur";
-                          }
-                          else {
-                            null;
-                          }
-                        },
-                      ),
-                      TextButton(
-                          onPressed: ()
-                          {
-                            if (_formKey.currentState!.validate()) {
-                              FocusScope.of(context).unfocus();
-                            } else {
-                              print("Nope");
-                            };
-                          },
-                          child: const Text("Valider")
-                      )
-                    ],
-                  )
+                Text("Valeur du switch :${isOn ? "green" : "blue"}"),
+                Switch(
+                  value: isOn,
+                  onChanged: (newBool){
+                    setState(() {isOn = newBool;})
+                    ;},
                 )
               ],
             ),
-          )
-        ),
+            Row (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children : [
+                Text("${_current.toInt()} / 6")
+              ]
+            ),
+            Row (
+              children : [
+                Text (_min.toString()),
+                Expanded(
+                    child : Slider(
+                        divisions: 6,
+                        min: _min,
+                        max: _max,
+                        value: _current,
+                        onChanged: (newValue) {
+                          setState(() {
+                            _current = newValue;
+                          });
+                        }
+                    )
+                ),
+                Text (_max.toString()),
+              ],
+            ),
+          ],
+        )
       )
     );
   }
